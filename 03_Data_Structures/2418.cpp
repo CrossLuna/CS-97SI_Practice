65d4deb635cb50c630ea7f3702e57fb45ed2d62c
Chao-Yang Lo <lo.chao.yang@gmail.com>
2017-01-23 10:27:36 +0100

    Correst 2418 spaces/tabs

--- 03_Data_Structures/2418.cpp
+++ 03_Data_Structures/2418.cpp
@@ -3,21 +3,21 @@
 #include <map>
 using namespace std;
 int main() {
-	ios::sync_with_stdio(false);
-	map<string, int> mm;
-	int cnt = 0;
-	string str;
-	while(getline(cin, str)) {
-		if(mm.find(str) != mm.end()) {
-			mm[str]++;
-		}
-		else {
-			mm[str] = 1;
-		}
-		cnt++;
-	}
-	for(map<string, int>::iterator it = mm.begin(); it != mm.end(); ++it){
-		printf("%s %.4f\n",(it->first).c_str() , 100.0*static_cast<double>(it->second)/cnt);
-	}
-	return 0;
+    ios::sync_with_stdio(false);
+    map<string, int> mm;
+    int cnt = 0;
+    string str;
+    while(getline(cin, str)) {
+        if(mm.find(str) != mm.end()) {
+            mm[str]++;
+        }
+        else {
+            mm[str] = 1;
+        }
+        cnt++;
+    }
+    for(map<string, int>::iterator it = mm.begin(); it != mm.end(); ++it){
+        printf("%s %.4f\n",(it->first).c_str() , 100.0*static_cast<double>(it->second)/cnt);
+    }
+    return 0;
 }
